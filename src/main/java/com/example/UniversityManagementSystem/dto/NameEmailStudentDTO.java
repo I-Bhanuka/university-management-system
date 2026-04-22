04d9caf756b4b9214acf48b40f606fdc292c50c0
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 14:36:12 +0530

    Add NameEmailStudentDTO for custom search

---
+++ src/main/java/com/example/UniversityManagementSystem/dto/NameEmailStudentDTO.java
@@ -0,0 +1,13 @@
+package com.example.UniversityManagementSystem.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NameEmailStudentDTO {
+    private String firstName;
+    private String email;
+}
