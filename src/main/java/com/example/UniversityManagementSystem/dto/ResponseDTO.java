f55f98c177d967a2f76ddb5772a3f8b58dc7504b
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 10:48:15 +0530

    Create a general response dto

---
+++ src/main/java/com/example/UniversityManagementSystem/dto/ResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.UniversityManagementSystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseDTO {
+    private String message;
+}
