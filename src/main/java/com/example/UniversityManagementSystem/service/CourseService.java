22c0065c4008686bdeb892043bde9c6f4046baad
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 16:28:36 +0530

    Add findByCourseId and findAll course service methods

--- src/main/java/com/example/UniversityManagementSystem/service/CourseService.java
+++ src/main/java/com/example/UniversityManagementSystem/service/CourseService.java
@@ -1,7 +1,9 @@
 package com.example.UniversityManagementSystem.service;
 
+import com.example.UniversityManagementSystem.dto.CourseIdDTO;
 import com.example.UniversityManagementSystem.dto.RegisterCourseDTO;
 import com.example.UniversityManagementSystem.entity.Course;
+import com.example.UniversityManagementSystem.entity.Student;
 import com.example.UniversityManagementSystem.enums.CourseStatus;
 import com.example.UniversityManagementSystem.repository.CourseRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -45,4 +47,29 @@ public class CourseService {
     public List<Course> getAllCourses() {
         return courseRepository.findAll();
     }
+
+    // Read a course by ID method
+    public Course getCourseById(CourseIdDTO request) {
+        Course crs = getCourseByCourseId(request.getCourseId());
+
+        log.info("Course with ID {} found: {}", request.getCourseId(), crs);
+
+        return crs;
+
+    }
+
+    // Helper method to find the course by courseId
+    public Course getCourseByCourseId(String courseId) {
+        // Find the Course
+        Course crs = courseRepository.findByCourseId(courseId).orElse(null);
+
+        // If course is not found, log the error and throw an exception
+        if (crs == null){
+            log.info("Course with courseId {} not found", courseId);
+            throw new RuntimeException("Course not found");
+        }
+
+        return crs;
+    }
 }
+
