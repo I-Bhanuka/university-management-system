de01d321b5e8aca51e18c30456e804b3d53c7bc9
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 12:36:16 +0530

    Create the StudentRepository

---
+++ src/main/java/com/example/UniversityManagementSystem/repository/StudentRepository.java
@@ -0,0 +1,22 @@
+package com.example.UniversityManagementSystem.repository;
+
+import com.example.UniversityManagementSystem.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+
+    /**
+     * Query,
+     * SELECT *
+     * FROM student
+     * ORDER BY created_at DESC
+     * LIMIT 1;
+     */
+    Optional<Student> findTopByOrderByCreatedAtDesc();
+}
