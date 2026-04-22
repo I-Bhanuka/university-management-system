c5864d5e7dac24f6099e6fd3dda975d0bcc8c3e6
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 10:52:29 +0530

    Add modifications to the register student enpoint resource location

--- src/main/java/com/example/UniversityManagementSystem/Controller/StudentController.java
+++ src/main/java/com/example/UniversityManagementSystem/Controller/StudentController.java
@@ -1,6 +1,11 @@
 package com.example.UniversityManagementSystem.Controller;
 
+import com.example.UniversityManagementSystem.dto.RegisterStudentDTO;
+import com.example.UniversityManagementSystem.dto.ResponseDTO;
+import com.example.UniversityManagementSystem.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController // Marks that this class handles http requests and automatically make the return object json
@@ -8,15 +13,23 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class StudentController {
 
+    private final StudentService studentService;
+
+    // Dependency injection - Constructor injection
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public String getAllStudents() {
         return "List of all students";
     }
 
-    @PostMapping("register")
-    public String registerStudent(@RequestBody Object object) {
-        log.info("Register student {}", object);
-        return "Create a new student. Name: " + object.toString();
+    @PostMapping()
+    public ResponseEntity<ResponseDTO> registerStudent(@Valid @RequestBody RegisterStudentDTO request) {
+        studentService.registerStudent(request);
+
+        return ResponseEntity.ok(new ResponseDTO("Student created Successfully"));
     }
 
 }
