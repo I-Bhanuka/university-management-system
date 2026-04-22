a7208191554caae68ef2af2bbe427fbd2b69b261
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 10:51:26 +0530

    Add auto-increment studentId logic into StudentService class

--- src/main/java/com/example/UniversityManagementSystem/service/StudentService.java
+++ src/main/java/com/example/UniversityManagementSystem/service/StudentService.java
@@ -12,7 +12,38 @@ public class StudentService {
 
     public void registerStudent(RegisterStudentDTO request) {
 
-        log.info("Register Student Request");
+        // convert the String DOB into LocalDate
+        LocalDate date = LocalDate.parse(request.getDob());
+        // Get the Enrollment date
+        LocalDate enrollmentDate = LocalDate.now();
+
+        // Create the StudentId
+        String studentId = "ST-20260000";
+
+        // Split everything, assuming that studentId is retrieved from database and is in the format "ST-20260000"
+        String[] parts = studentId.split("-");
+        String prefix = parts[0]; // "ST"
+        int yearId = Integer.parseInt(parts[1].subSequence(0, 4).toString()); // "2026"
+
+        // Get the sub string, parse into int, and then add 1 to it
+        int numberId = Integer.parseInt(parts[1].substring(4)) + 1; // "0000"
+
+        // Check the year portion of the ID
+        if (yearId != LocalDate.now().getYear()) {
+            // If the year portion does not match with the current year, update the year
+            yearId = LocalDate.now().getYear();
+
+            // Reset the number portion to 0000
+            numberId = 0;
+        }
+
+        // Construct the new studentId
+        studentId = String.format("%s-%d%04d", prefix, yearId, numberId); // "ST-20260001";
+        log.info("Student ID: {}", studentId);
+
+
+
+        // Create the Student entity and save to database
     }
 
 }
