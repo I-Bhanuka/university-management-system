d85f6f89654a1911850a90b8ef2b5833c96464e3
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-23 10:08:56 +0530

    Add the deactivateCourseById controller method to de-activate a course

--- src/main/java/com/example/UniversityManagementSystem/Controller/CourseController.java
+++ src/main/java/com/example/UniversityManagementSystem/Controller/CourseController.java
@@ -2,12 +2,10 @@ package com.example.UniversityManagementSystem.Controller;
 
 import com.example.UniversityManagementSystem.dto.CourseIdDTO;
 import com.example.UniversityManagementSystem.dto.RegisterCourseDTO;
-import com.example.UniversityManagementSystem.dto.RegisterStudentDTO;
 import com.example.UniversityManagementSystem.dto.ResponseDTO;
 import com.example.UniversityManagementSystem.entity.Course;
 import com.example.UniversityManagementSystem.service.CourseService;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +17,7 @@ import java.util.List;
 @RequestMapping("/api/courses")
 public class CourseController {
 
-    private CourseService courseService;
+    private final CourseService courseService;
 
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
@@ -36,17 +34,29 @@ public class CourseController {
 
 
     // Read all courses
-    @GetMapping
+    @PostMapping("/all")
     public List<Course> getAllCourses() {
         return courseService.getAllCourses();
     }
 
     // Read a course by ID
-    @GetMapping("/getCourseById")
+    @PostMapping("/getCourseById")
     public Course getCourseById(@Valid @RequestBody CourseIdDTO request) {
         return courseService.getCourseById(request);
     }
 
     // De-activating a course
+    @PostMapping("/deactivate")
+    public ResponseEntity<ResponseDTO> deactivateCourseById(@Valid @RequestBody CourseIdDTO request) {
+        courseService.deactivateCourseById(request);
+
+        return ResponseEntity.ok(new ResponseDTO("Course deactivated successfully"));
+    }
+
+    // Update course details
+
+    // Enroll students in a course
+
+    // Assign lecturers to a course
 
 }
