5a2780662e70a55005d1e6a77da35711523bb8d5
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-27 16:29:18 +0530

    Add Custom JPQL query into course repositroy to get courses with the number of students enrolled in them0

--- src/main/java/com/example/UniversityManagementSystem/repository/CourseRepository.java
+++ src/main/java/com/example/UniversityManagementSystem/repository/CourseRepository.java
@@ -2,10 +2,24 @@ package com.example.UniversityManagementSystem.repository;
 
 import com.example.UniversityManagementSystem.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
     Optional<Course> findByCourseId(String courseId);
+
+
+    // Custom JPQL query to find course they are enrolled in
+
+    // Count students by the course they are enrolled in
+    @Query("""
+        SELECT c.courseName, COUNT(s)
+        FROM Course c
+        JOIN Student s ON s.course.id = c.id
+        GROUP BY c.courseName
+    """)
+    List<Object[]> countStudentsByCourse();
 }
