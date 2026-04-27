2243494cc20e22835a267e2e3fd5e1fa3cba6857
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-27 09:16:10 +0530

    Add AuthController to deal with the registration and login endpoints

---
+++ src/main/java/com/example/UniversityManagementSystem/Controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.example.UniversityManagementSystem.Controller;
+
+import com.example.UniversityManagementSystem.dto.JwtResponseDTO;
+import com.example.UniversityManagementSystem.dto.LoginUserRequestDTO;
+import com.example.UniversityManagementSystem.dto.RegisterUserRequestDTO;
+import com.example.UniversityManagementSystem.dto.ResponseDTO;
+import com.example.UniversityManagementSystem.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> register(@Valid @RequestBody RegisterUserRequestDTO request) {
+        authService.register(request);
+
+        return ResponseEntity.ok(new ResponseDTO("User registered successfully"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponseDTO> login(@Valid @RequestBody LoginUserRequestDTO request) {
+        JwtResponseDTO response = authService.login(request);
+
+        return  ResponseEntity.ok(response);
+
+
+    }
+
+}
