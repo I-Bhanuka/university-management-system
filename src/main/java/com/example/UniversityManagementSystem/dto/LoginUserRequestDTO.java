d9c5e5c938af94cd329cade02a7c447e3916bacb
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-27 09:14:37 +0530

    Add LoginUserRequestDTO to help with user login process

---
+++ src/main/java/com/example/UniversityManagementSystem/dto/LoginUserRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.UniversityManagementSystem.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginUserRequestDTO {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
