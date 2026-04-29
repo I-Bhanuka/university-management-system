e10f313c003b5eeb23b4746a681f453f75567498
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-29 09:08:10 +0530

    refactor: add Builder functionalities into StudentIdDTO

--- src/main/java/com/example/UniversityManagementSystem/dto/StudentIdDTO.java
+++ src/main/java/com/example/UniversityManagementSystem/dto/StudentIdDTO.java
@@ -1,15 +1,13 @@
 package com.example.UniversityManagementSystem.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class StudentIdDTO {
 
     @NotBlank(message = "Student ID is required")
